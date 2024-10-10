1770326ad88722e91af3439766c3f914a82c8027
Beltru <147621160+Beltru@users.noreply.github.com>
2024-10-10 15:05:40 -0300

    login y register terminados

--- mbn/app/login/page.jsx
+++ mbn/app/login/page.jsx
@@ -7,33 +7,35 @@ export default function Login() {
         <div className="bg-gradient-to-r from-[#080c2c] via-[#0d1449] to-black overflow-hidden">
            <main className="">
             <section className="h-[100vh] w-[100vw] flex justify-between items-center">
-              <div className="ml-[15vw]"> 
-                <Image src={"/mbn.png"}
-                    width={280}
-                    height={280}/>
-              </div>
+                <div className="h-[100vh] w-[60vw] flex justify-end items-center pr-[17%]"> 
+                    <a href="/" className="cursor-pointer"><Image src={"/mbn.png"} width={280} height={280} /></a>
+                </div>
                 
-                <div className="flex justify-center items-center rounded-xl w-[40vw] h-[100vw] bg-black hover:w-[55vw] transition-all duration-500 rounded-l-lg">
-                    <form action="#" className="w-[50%] ">
+                <div className="flex justify-center items-center rounded-xl w-[40vw] h-[100vh] bg-black hover:w-[85vw] transition-all duration-500 rounded-l-lg">
+                    <form action="#" className="w-[50%] flex h-[50vh] justify-around">
                         <h2>Login</h2>
-                        <div className="input-field">
-                            <input type="text" required/>
-                            <label>Email</label>
+                        <div>
+                            <div className="input-field mb-[1.5vw]">
+                                <input type="text" required/>
+                                <label>Email</label>
+                            </div>
+                            <div className="input-field mb-[1.5vw]">
+                                <input type="password" required/>
+                                <label>Password</label>
+                            </div>
+                            <div className="forget">
+                                <label for="remember">
+                                    <input type="checkbox" id="remember"/>
+                                    <p>Remember me</p>
+                                </label>
+                                <a href="http://localhost:3000/forgot">Forgot password?</a>
+                            </div>
                         </div>
-                        <div className="input-field">
-                            <input type="password" required/>
-                            <label>Password</label>
-                        </div>
-                        <div className="forget">
-                            <label for="remember">
-                                <input type="checkbox" id="remember"/>
-                                <p>Remember me</p>
-                            </label>
-                            <a href="http://localhost:3000/forgot">Forgot password?</a>
-                        </div>
-                        <button type="submit">Log In</button>
-                        <div className="register">
-                            <p>Don't have an account?</p> <p><a href="http://localhost:3000/register" className="hover:text-[#ffffffe5] transition-all duration-300">Register</a></p>
+                        <div className="flex justify-center items-center flex-col w-[100%]">
+                            <button type="submit" className="w-[80%]">Log In</button>
+                            <div className="register">
+                                <p>Don't have an account?</p> <p><a href="http://localhost:3000/register" className="hover:text-[#ffffff] transition-all duration-300">Register</a></p>
+                            </div>
                         </div>
                     </form>
                 </div>
--- mbn/app/register/page.jsx
+++ mbn/app/register/page.jsx
@@ -25,44 +25,48 @@ export default function Register() {
         <div className="bg-gradient-to-r from-[#080c2c] via-[#0d1449] to-black overflow-hidden">
             <main>
                 <section className="h-[100vh] w-[100vw] flex justify-between items-center">
-                    <div className="ml-[15vw]"> 
-                        <Image src={"/mbn.png"} width={280} height={280} />
+                    <div className="h-[100vh] w-[60vw] flex justify-end items-center pr-[17%]"> 
+                        <a href="/" className="cursor-pointer"><Image src={"/mbn.png"} width={280} height={280} /></a>
                     </div>
                     
-                    <div className="flex justify-center items-center rounded-xl w-[40vw] h-[100vw] bg-black hover:w-[55vw] transition-all duration-500 rounded-l-lg">
-                        <form onSubmit={handleSubmit} className="w-[50%]">
-                            <h2>Register</h2>
-                            <div className="input-field">
-                                <input type="text" required />
-                                <label>Name</label>
+                    <div className="flex justify-center items-center rounded-xl w-[40vw] h-[100vh] bg-black hover:w-[85vw] transition-all duration-500 rounded-l-lg">
+                        <form onSubmit={handleSubmit} className="w-[50%] flex h-[65vh] justify-around">
+                            <h2>Create an Account</h2>
+                            <div>
+                                <div className="input-field mb-[1.5vw]">
+                                    <input type="text" required />
+                                    <label>Name</label>
+                                </div>
+                                <div className="input-field mb-[1.5vw]">
+                                    <input type="text" required />
+                                    <label>Email</label>
+                                </div>
+                                <div className="input-field mb-[1.5vw]">
+                                    <input
+                                        type="password"
+                                        required
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                    <label className="absolute">Password</label>
+                                </div>
+                                <div className="input-field mb-[1.5vw]">
+                                    <input
+                                        type="password"
+                                        required
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                    />
+                                    <label>Confirm password</label>
+                                </div>
+                                {error && <p className="text-red-500 py-1 mb-2">{error}</p>}
                             </div>
-                            <div className="input-field">
-                                <input type="text" required />
-                                <label>Email</label>
-                            </div>
-                            <div className="input-field">
-                                <input
-                                    type="password"
-                                    required
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                                <label className="absolute">Password</label>
-                            </div>
-                            <div className="input-field">
-                                <input
-                                    type="password"
-                                    required
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                />
-                                <label>Confirm password</label>
-                            </div>
-                            {error && <p className="text-red-500 py-1 mb-2">{error}</p>}
-                            <button type="submit" className="submit">Register</button>
-                            <div className="register">
-                                <p>Already have an account?</p>
-                                <p><a href="http://localhost:3000/login">Log in</a></p>
+                            <div className="flex justify-center items-center flex-col w-[100%]">
+                                <button type="submit" className="submit w-[80%]">Register</button>
+                                <div className="register">
+                                    <p className="text-[#ccc]">Already have an account?</p>
+                                    <p><a href="http://localhost:3000/login" className="text-[#ccc] hover:text-[#fff] transition-all duration-300">Log in</a></p>
+                                </div>
                             </div>
                         </form> 
                     </div>
