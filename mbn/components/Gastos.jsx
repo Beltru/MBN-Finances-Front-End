505b0e9df25b48d1cfe945a8208ef8a88d7d0601
Beltru <belvader007@gmail.com>
2024-10-16 08:37:16 -0300

    haciendo modal

--- mbn/components/Gastos.jsx
+++ mbn/components/Gastos.jsx
@@ -1,36 +1,10 @@
 "use client";
 import Modal from "../components/Modal";
-
+import Gastos_Dia from "./Gastos_dia"
 import { useEffect, useRef, useState } from 'react';
 
-const Gastos_Dia = ({ dia, gastos }) => {
-  const barRef = useRef(null);
-
-  useEffect(() => {
-    if (barRef.current) {
-      barRef.current.style.height = '0px'; // Start with zero height
-      setTimeout(() => {
-        barRef.current.style.height = `${gastos * 5}px`; // Animate to final height
-      }, 300); // Adjust delay as needed
-    }
-  }, [gastos]);
-
-  return (
-    <div className="space-y-1">
-      <p>{dia}</p>
-      <div className="flex flex-col-reverse">
-        <div
-          ref={barRef}
-          className="bg-blue-600 rounded-lg transition-height duration-500 ease-in-out"
-          style={{ width: '22px' }}
-        ></div>
-      </div>
-    </div>
-  );
-};
-
   const Gastos = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
--- mbn/components/Modal.jsx
+++ mbn/components/Modal.jsx
@@ -1,20 +1,32 @@
 "use Client"
 import { useState } from 'react';
+import Grafico from "./Grafico";
+import Gastos_Dia from "./Gastos_dia"
 
 export default function Modal({ isOpen, onClose }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-2xl mb-4">Modal Title</h2>
-        <p className="mb-4">This is the content of the modal.</p>
-        <button
-          onClick={onClose}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Close Modal
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 ">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-[50vw]">
+         <div className='flex flex-row justify-between'>
+        <h2 className="text-2xl mb-1">Gastos</h2>
+        <button onClick={onClose} className="px-3 bg-gray-700 text-white rounded hover:bg-gray-600">
+          X
         </button>
+         </div>
+         
+        <div className="flex justify-between mt-4">
+          <Gastos_Dia dia="Lun" gastos="24" />
+          <Gastos_Dia dia="Mar" gastos="12" />
+          <Gastos_Dia dia="Mie" gastos="6" />
+          <Gastos_Dia dia="Jue" gastos="8" />
+          <Gastos_Dia dia="Vie" gastos="4" />
+          <Gastos_Dia dia="Sab" gastos="22" />
+          <Gastos_Dia dia="Dom" gastos="20" />
+        </div>
+        
+       
       </div>
     </div>
   );
