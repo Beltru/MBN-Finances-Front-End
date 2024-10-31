a516c7536a97f2b7d9ac6b7bea41cbcce7fb0475
Beltru <belvader007@gmail.com>
2024-10-31 11:35:40 -0300

    ciruclo de money

--- mbn/app/home/page.jsx
+++ mbn/app/home/page.jsx
@@ -15,29 +15,26 @@ export default function Home() {
       <nav>
         <Navbar/>
       </nav>
-      <main className="flex justify-center items-end w-[100vw] h-[85vh]">
+      <main className="flex justify-center items-end w-[100vw] h-[87vh]">
 
         <section className="grid grid-cols-12 gap-x-[7vw] gap-y-[5vh] w-[70vw]">
 
 
           {/* Caja de Dinero en Cuenta */}
-          <div className="row-span-1 col-span-3 ml-[-25%] flex flex-col items-center justify-between">
-            <div className='flex items-center bg-neutral-900 border-[#3c6edb] border-[12px] rounded-full w-[55%] h-[43%]'>
-              <DineroCuenta name={"Dinero"} number={145706}/>
+          <div className="row-span-1 col-span-4 col-start-1  flex flex-col items-center justify-between py-8 px-6">
+            <div className='flex items-center bg-neutral-900 border-[#3c6edb] border-[12px] rounded-full w-[100%] h-[100%] justify-center'>
+              <DineroCuenta name={"Money Available"} number={145706}/>
             </div> 
-            <div className='flex items-center bg-neutral-900 border-[#cf318d] border-[12px] rounded-full w-[55%] h-[43%]'>
-              <DineroCuenta name={"Ingresos"} number={42000}/>
-            </div>    
           </div>
 
           {/* Caja de Gastos */}
-          <div className="mr-[1.8vw] col-span-8 rounded-lg flex flex-col">
+          <div className="mr-[1.8vw] col-start-5 col-span-8 rounded-lg flex flex-col">
             <Gastos/>
           </div>
    
 
           {/* Caja de Resumen */}
-          <div className="col-span-4 mt-[2vw]">
+          <div className="col-span-4 mt-[1vw]">
             <Resumen
               mes={"August"}
               progress1={50}
@@ -47,7 +44,7 @@ export default function Home() {
 
           
           {/* Caja de Gráfico */}
-          <div className="col-span-8 rounded-lg mr-[1.8vw] mt-[2vw]">
+          <div className="col-span-8 rounded-lg mr-[1.8vw] mt-[1vw]">
             <div className="">
                 <Chart/>
             </div>
