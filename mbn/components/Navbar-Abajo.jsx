0989e0b53c21b5faf2f6e34dcb1f717624c490d4
Beltru <belvader007@gmail.com>
2024-11-26 14:15:58 -0300

    arreglo landing para compu chica

--- mbn/components/Navbar-Abajo.jsx
+++ mbn/components/Navbar-Abajo.jsx
@@ -1,90 +1,92 @@
-"use client"
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const Navbar = () => {
-    const [activeCard, setActiveCard] = useState(1);
+const Navbar = ({ isFooterVisible }) => {
+  const [activeCard, setActiveCard] = useState(1);
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        
-        if (scrollPosition < 2000) {
-          setActiveCard(1);
-        } else if (scrollPosition >= 2000 && scrollPosition < 2700) {
-          setActiveCard(2);
-        } else if (scrollPosition >= 2700) {
-          setActiveCard(3);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-    return(
-    <nav className="flex justify-between flex-row items-center fixed bottom-[5vh] mx-[20vw] bg-[rgb(26,26,26,0.8)] w-[55vw] rounded-xl px-4 py-1 z-50">
-        <div className="flex flex-row items-center">
-          <div>
-            <Image
-              src={"/mbn.png"}
-              alt=""
-              width={55}
-              height={55}
-            />
-          </div>
-          <div className="divider">
-            <hr className="line mx-2 mr-2"/>
-          </div>
-          <ul className="flex flex-row">
-            <li className={`p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1 ${activeCard === 1 ? "bg-[#303030]" : ""}`}>
-              <Image 
-                src={"/bank.svg"}
-                alt=""
-                width={18}
-                height={18}
-              />
-            </li>
-            <li className={`p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1 ${activeCard === 2 ? "bg-[#303030]" : ""}`}>
-              <Image 
-                src={"/graph-up.svg"}
-                alt=""
-                width={18}
-                height={18}
-              />
-            </li>
-            <li className={`p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1 ${activeCard === 3 ? "bg-[#303030]" : ""}`}>
-              <Image 
-                src={"/wallet2.svg"}
-                alt=""
-                width={18}
-                height={18}
-              />
-            </li>
-          </ul>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      if (scrollPosition < 2000) {
+        setActiveCard(1);
+      } else if (scrollPosition >= 2000 && scrollPosition < 2700) {
+        setActiveCard(2);
+      } else if (scrollPosition >= 2700) {
+        setActiveCard(3);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`flex justify-between flex-row items-center fixed bottom-[5vh] mx-[20vw] bg-[rgb(26,26,26,0.8)] w-[55vw] rounded-xl px-4 py-1 z-50 transition-transform duration-500 ${
+        isFooterVisible ? "translate-y-[100px] opacity-0" : "translate-y-0 opacity-100"
+      }`}
+    >
+      <div className="flex flex-row items-center">
+        <div>
+          <Image src={"/mbn.png"} alt="" width={55} height={55} />
         </div>
-        <div className="flex flex-row items-center">
-          <ul className="flex flex-row">
-            <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem]">
-              Team
-            </li>
-            <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem]">
-              Support
-            </li>
-            <Link href="/login" className="px-2 py-1 rounded-lg flex items-center justify-center hover:hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem]">
-              Login
-            </Link>
-            <Link href="/register" className=" px-3 py-1 rounded-lg flex items-center justify-center bg-red-600 hover:hover:bg-red-700 duration-500 cursor-pointer mr-2 text-[0.95rem]">
-              Sign Up
-            </Link> 
-          </ul>
+        <div className="divider">
+          <hr className="line mx-2 mr-2" />
         </div>
-      </nav>
-    )
-}
+        <ul className="flex flex-row">
+          <li
+            className={`p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1 ${
+              activeCard === 1 ? "bg-[#303030]" : ""
+            }`}
+          >
+            <Image src={"/bank.svg"} alt="" width={18} height={18} />
+          </li>
+          <li
+            className={`p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1 ${
+              activeCard === 2 ? "bg-[#303030]" : ""
+            }`}
+          >
+            <Image src={"/graph-up.svg"} alt="" width={18} height={18} />
+          </li>
+          <li
+            className={`p-2 rounded-lg flex items-center justify-center hover:bg-[#303030] transition-all duration-500 cursor-pointer mx-1 ${
+              activeCard === 3 ? "bg-[#303030]" : ""
+            }`}
+          >
+            <Image src={"/wallet2.svg"} alt="" width={18} height={18} />
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-row items-center">
+        <ul className="flex flex-row">
+          <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem]">
+            Team
+          </li>
+          <li className="px-2 py-1 rounded-lg flex items-center justify-center hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem]">
+            Support
+          </li>
+          <Link
+            href="/login"
+            className="px-2 py-1 rounded-lg flex items-center justify-center hover:bg-[#303030] duration-500 cursor-pointer mr-4 text-[0.95rem]"
+          >
+            Login
+          </Link>
+          <Link
+            href="/register"
+            className="px-3 py-1 rounded-lg flex items-center justify-center bg-red-600 hover:bg-red-700 duration-500 cursor-pointer mr-2 text-[0.95rem]"
+          >
+            Sign Up
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
