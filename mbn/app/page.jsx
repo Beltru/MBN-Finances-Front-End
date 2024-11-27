cff8c14d411d041de38a857bf8c4a393104bfbc2
Beltru <belvader007@gmail.com>
2024-11-27 15:26:21 -0300

    Update page.jsx

--- mbn/app/page.jsx
+++ mbn/app/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from 'react';
+import { useState, useEffect, useRef } from "react";
 import ScrollCards from "../components/Cards-Scroll";
 import Navbar from "../components/Navbar-Abajo"
 import Card from "../components/Carrousel"
